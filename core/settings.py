08b14b01d68e5dccd34a2eec31849246c6dc21ea
Start Soft <StartSoft@ItTopNSK.local>
2026-04-23 21:11:38 +0700

    fix db

--- core/settings.py
+++ core/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shop_db',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
